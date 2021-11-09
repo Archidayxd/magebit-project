65501f5748b6e56c51aa4338650bb916543684e7
Artjoms Atroscenko <Atroscenko@yahoo.com>
2021-11-09 11:20:42 +0200

    Added semantic to Html / Added text to first page

--- index.php
+++ index.php
@@ -9,7 +9,46 @@
 </head>
 
 <body>
-
+    <div class="container">
+        <div class="subscription">
+            <header class="header">
+                <nav class="navigation">
+                    <div class="navigation__logo">
+                        <div class="navigation__image"><img src="/images/logo.svg" alt="Pineapple logo image"></div>
+                        <div class="navigation__name"><img src="/images/pineapple.svg" alt="pineapple logo name"></div>
+                    </div>
+                    <div class="navigation__items">
+                        <ul>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">How it works</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+            <main>
+                <div class="subscription__about">
+                    <div class="subscription__title">Subscribe to newsletter</div>
+                    <div class="subscription__subtitle">Subscribe to our newsletter and get 10% discount on pineapple glasses.</div>
+                    <div class="subscription__form"></div>
+                    <div class="subscription__terms">
+                        <div class="subscription__checkbox"></div>
+                        <div class="subscription__terms"></div>
+                    </div>
+                </div>
+            </main>
+            <div class="subscription__line"></div>
+            <footer>
+                <div class="social-profiles">
+                    <div class="social-items"></div>
+                    <div class="social-items"></div>
+                    <div class="social-items"></div>
+                    <div class="social-items"></div>
+                </div>
+            </footer>
+        </div>
+        <div class="bg-image"></div>
+    </div>
 </body>
 
 </html>
