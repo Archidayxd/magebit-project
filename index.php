325e2cb75c090d64cd5000fc70239f0e9a69e72f
Artjoms Atroscenko <Atroscenko@yahoo.com>
2021-11-09 16:48:48 +0200

    Updated html / css structure

--- index.php
+++ index.php
@@ -1,51 +1,53 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pineapple</title>
-    <link rel="stylesheet" href="/styles/style.css">
+    <title>Contact us</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0">
 </head>
 
 <body>
     <div class="container">
-        <div class="subscription">
-            <header class="header">
-                <nav class="navigation">
-                    <ul>
-                        <li class="left"><a href="#"></a><img src="/images/logo.svg" alt="Pineapple logo image"></li>
-                        <li class="left"><a href="#"></a><img src="/images/pineapple.svg" alt="pineapple logo name"></li>
-                        <li class="right"><a href="#">About</a></li>
-                        <li class="right"><a href="#">How it works</a></li>
-                        <li class="right"><a href="#">Contact</a></li>
-                    </ul>
-                </nav>
-            </header>
-            <main>
-                <div class="subscription__about">
-                    <div class="subscription__title">Subscribe to newsletter</div>
-                    <div class="subscription__subtitle">Subscribe to our newsletter and get 10% discount on pineapple glasses.</div>
-                    <div class="subscription__form"></div>
-                    <div class="subscription__terms">
-                        <div class="subscription__checkbox"></div>
-                        <div class="subscription__terms"></div>
+        <div class="contact-box">
+            <div class="left">
+                <header class="header">
+                    <nav class="navigation">
+                        <ul>
+                            <li><a href=""><img src="/img/logo.svg" alt=""></a></li>
+                            <li><a href=""><img src="/img/pineapple.svg" alt=""></a></li>
+                            <li><a href="">About</a></li>
+                            <li><a href="">How it works</a></li>
+                            <li><a href="">Contact</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                <main>
+                    <div class="subscription__about">
+                        <div class="subscription__title">Subscribe to newsletter</div>
+                        <div class="subscription__subtitle">Subscribe to our newsletter and get 10% discount on pineapple glasses.</div>
+                        <div class="subscription__form"></div>
+                        <div class="subscription__terms">
+                            <div class="subscription__checkbox"></div>
+                            <div class="subscription__terms"></div>
+                        </div>
                     </div>
-                </div>
-            </main>
-            <div class="subscription__line"></div>
-            <footer>
-                <div class="social-profiles">
-                    <div class="social-items"></div>
-                    <div class="social-items"></div>
-                    <div class="social-items"></div>
-                    <div class="social-items"></div>
-                </div>
-            </footer>
+                </main>
+                <div class="subscription__line"></div>
+                <footer>
+                    <div class="social-profiles">
+                        <div class="social-items"></div>
+                        <div class="social-items"></div>
+                        <div class="social-items"></div>
+                        <div class="social-items"></div>
+                    </div>
+                </footer>
+            </div>
+            <div class="right">
+            </div>
         </div>
     </div>
-    <div class="bg-image"></div>
 </body>
 
 </html>
