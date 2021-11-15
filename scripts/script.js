3f5dfbe1b490a5ae51b25a269d86070b3f0ec0aa
Artjoms Atroscenko <Atroscenko@yahoo.com>
2021-11-15 12:36:28 +0200

    removed unnecessary in the script

--- scripts/script.js
+++ scripts/script.js
@@ -1,54 +1,48 @@
-let form = document.getElementById("form")
+let form = document.getElementById("form");
 let errorElement = document.getElementById("error")
-form.classList.add("valid")
+form.classList.add("valid");
+let emailCheck = document.getElementById("email");
+let checkBox = document.getElementById("checkBox");
+let subButton = document.getElementById("subButton");
 
 window.onload = function() {
-    document.getElementById('email').value = "";
-    document.getElementById("checkBox").checked = false;
+    emailCheck.value = "";
+    checkBox.checked = false;
     }
 
 function checkEmail(){
 
-let emailCheck = document.getElementById("email").value;
 
 let pattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 
-if(emailCheck.match(pattern)){
+if(emailCheck.value.match(pattern)){
     form.classList.add("valid")
     form.classList.remove("invalid")
-    document.getElementById("subButton").disabled = false
-    document.getElementById("error").textContent = ""
-    if(/.co\s*$/.test(emailCheck)){
+    subButton.disabled = false
+    errorElement.textContent = ""
+    if(/.co\s*$/.test(emailCheck.value)){
         form.classList.add("invalid")
         form.classList.remove("valid")
-        document.getElementById("subButton").disabled = true;
-        document.getElementById("error").textContent = "We are not accepting subscriptions from Colombia emails"
+        subButton.disabled = true;
+        errorElement.textContent = "We are not accepting subscriptions from Colombia emails"
     }
 } else{
     form.classList.add("invalid")
     form.classList.remove("valid")
-    document.getElementById("subButton").disabled = true;
-    document.getElementById("error").textContent = "Please provide a valid e-mail address"
+    subButton.disabled = true;
+    errorElement.textContent = "Please provide a valid e-mail address"
 }
 }
-
-// function termsChanged(){
-//     if(document.getElementById("checkBox").checked){
-//         document.getElementById("subButton").disabled = false;
-//     } else{
-//         document.getElementById("subButton").disabled = true;
-//     }
-// }
 
 form.addEventListener("submit" , (e) =>{
     let errorMessages=[]
-    if(document.getElementById("email").value == "" || document.getElementById("email").value==null){
+    if(emailCheck.value == "" || emailCheck.value==null){
         errorMessages.push("Please provide a valid e-mail address")
         // console.log(errorMessages)
     }
 
-    if(!document.getElementById("checkBox").checked){
+    if(!checkBox.checked){
         errorMessages.push("You must accept the terms and conditions")
     }
 
@@ -59,11 +53,3 @@ form.addEventListener("submit" , (e) =>{
         form.classList.remove("valid")
     }
 })
-
-// function subButtonTest(event){
-//     if(document.getElementById("email").value == "" || document.getElementById("email").value==null){
-//     document.getElementById("error").textContent = "Please provide a valid e-mail address"
-//     console.log("yes")
-    
-//     }
-// }
