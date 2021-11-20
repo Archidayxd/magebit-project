ca8d718efea0153803eb927cee0e3b89cad691f6
Artjoms Atroscenko <Atroscenko@yahoo.com>
2021-11-20 13:01:08 +0200

    Added fonts folder / added span to error message

---
+++ components/layout.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- eto vsya stranica -->
+    <fluid-container>
+        <!-- tut nuzhen flex iz dvuh blokov left i right -->
+        <left>
+            <header>
+                <? require("components/header.php"); ?>
+            </header>
+            <form action=""></form>
+            <footer></footer>
+        </left>
+        <right>
+            <!-- Kartinka -->
+        </right>
+    </fluid-container>
+</body>
+
+</html>
--- components/subscription_box.php
+++ components/subscription_box.php
@@ -3,14 +3,17 @@
         <div class="media-wrapper">
             <div class="subscription__title sub-wrapper" id="title"><span>Subscribe to newsletter</span></div>
             <div class="subscription__subtitle sub-wrapper" id="subtitle"><span>Subscribe to our newsletter and get 10% discount on pineapple glasses.</span></div>
+
             <div class="subscription__input">
+                <span id="error">Please provide a valid e-mail address</span>
                 <form id="form" action="/phpbase/GetData.php" method="POST" class="form">
+
                     <input type="email" placeholder="Type your email address here…" name="email" id="email" onkeyup="checkEmail()">
                     <button type="submit" name="subscribe" id="subButton" onclick="subButtonTest()">
                         <span class="icon-arrow-right2"></span>
                     </button>
-                    <span id="error"></span>
-                    <div class="subscription__terms sub-wrapper">
+
+                    <div class="subscription__terms terms-wrapper">
                         <div class="subscription__checkbox">
                             <label class="subscription__checkbox-label"></label>
                             <input type="checkbox" class="checkbox" name="terms" id="checkBox" required onclick="termsChanged()">
@@ -18,8 +21,8 @@
                         </div>
                         <div class="subscription__service" id="checkboxLabel"><span>I agree to <a href="#">terms of service</a></span></div>
                     </div>
+                    <!-- <span id="error" class="sub-wrapper"></span> -->
                     <span id="status"></span>
                 </form>
             </div>
         </div>
-
