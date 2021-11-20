bed751d88dff07250836eb60c8b544f85676a190
Artjoms Atroscenko <Atroscenko@yahoo.com>
2021-11-20 18:35:02 +0200

    change to class structure

--- phpbase/download.php
+++ phpbase/download.php
@@ -1,41 +1,45 @@
 <?php
-$serverName = "localhost:3307";
-$userName = "foo";
-$password  = "bar";
-$dbName = "emails";
 
-$con = mysqli_connect($serverName, $userName, $password, $dbName);
+include("dbcon.php");
 
-if (!isset($_POST['xport']) or !is_array($_POST['xport'])) {
-    exit('No rows selected for export');
+class DownloadCsv extends dbcon
+{
+
+    public function download()
+    {
+        $con = $this->connect();
+
+        if (!isset($_POST['xport']) or !is_array($_POST['xport'])) {
+            exit('No rows selected for export');
+        }
+
+        $filename = "emails_list.csv";
+        $fp = fopen('php://output', 'w');
+
+        $sql = "SELECT * FROM email";
+        $result = $con->query($sql);
+        while ($row = mysqli_fetch_row($result)) {
+            $header[] = $row[0];
+        }
+
+        header('Content-type: application/csv');
+        header('Content-Disposition: attachment; filename=' . $filename);
+
+        $ids = $_POST['xport'];
+        array_walk($ids, function (&$value) {
+            $value = (int)$value;
+        });
+
+        $ids = implode(', ', $ids);
+
+        $query = "SELECT * FROM email WHERE Id IN ($ids)";
+        $result = $con->query($query);
+        while ($row = mysqli_fetch_row($result)) {
+            fputcsv($fp, $row);
+        }
+        exit;
+    }
 }
 
-$filename = "emails_list.csv";
-$fp = fopen('php://output', 'w');
-
-if ($con->connect_error) {
-    die("Connection failed: " . $con->connect_error);
-}
-
-$sql = "SELECT * FROM email";
-$result = $con->query($sql);
-while ($row = mysqli_fetch_row($result)) {
-    $header[] = $row[0];
-}
-
-header('Content-type: application/csv');
-header('Content-Disposition: attachment; filename=' . $filename);
-
-$ids = $_POST['xport'];
-array_walk($ids, function (&$value, $key) {
-    $value = (int)$value;
-});
-
-$ids = implode(', ', $ids);
-
-$query = "SELECT * FROM email WHERE Id IN ($ids)";
-$result = $con->query($query);
-while ($row = mysqli_fetch_row($result)) {
-    fputcsv($fp, $row);
-}
-exit;
+$DownloadCsv = new DownloadCsv;
+$DownloadCsv->download();
